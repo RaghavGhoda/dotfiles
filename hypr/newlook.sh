8032ca877949c73139463ff5f59fae4d10e7b0ca
Gurvinder Singh <133991448+Guru901@users.noreply.github.com>
2024-08-16 17:02:58 +0530

    .

--- hypr/newlook.sh
+++ hypr/newlook.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-wall=$(find $HOME/.walls -type f -name "*.jpg" -o -name "*.png" | shuf -n 1)
+wall=$(find $HOME/.walls -type f -name "*.jpg" -o -name "*.png" | shuf -n 1) 
 
-wal -q -i $wall
-swaybg -i $wall
-
-xdotool key super+F5
+wal -q -i $wall &
+swaybg -i $wall &
